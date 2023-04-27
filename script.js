e9c42459839dba0d4e39ce077714352831f66129
koechkaporin <billwizkid996@gmail.com>
2023-04-27 11:29:09 +0300

    rating component

---
+++ script.js
@@ -0,0 +1,14 @@
+const popup = document.getElementById("popup");
+function openPopup(){
+    popup.classList.add('open-modal');
+}
+function closePopup(){
+    popup.classList.remove('open-modal');
+    
+}
+
+function mouseOver() {
+    document.getElementById("mybtn").style.backgroundColor= "orange";
+  }
+
+  
